50ab5b2303626e45869bfcd04f9e7d26459f39be
Jess Gehring <34904627+JessG96@users.noreply.github.com>
2018-05-04 21:15:22 -0400

    Update Factorial_Python.py

--- Factorial_Python.py
+++ Factorial_Python.py
@@ -1,3 +1,11 @@
 def Factorial(Factorial_Number)
   if Factorial_Number < 1:
-      
+    
+    print("You can't use a negative number for factorial")
+    return
+  
+  elif Factorial_Number == 1:
+    return int(Factorial_Number * Factorial(Factorial_Number - 1))
+  
+Factorial_Number = int(input('Please Enter a Number: '))
+print(Factorial(Factorial_Number))
